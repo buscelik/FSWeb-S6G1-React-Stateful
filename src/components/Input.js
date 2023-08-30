6ce4b9de229b67efd5fe5ac7f3447be9abbbcd57
busracelik <124159666+buscelik@users.noreply.github.com>
2023-08-30 19:21:03 +0300

    React-Stateful

--- src/components/Input.js
+++ src/components/Input.js
@@ -33,35 +33,55 @@ ADIM 6:
   <input /> öğesine şu şekilde fazladan bir prop eklememiz gerekiyor: value={inputDeğeri}
 */
 
-import React from 'react'; /* ADIM 0 */
+import React, { useState } from "react"; /* ADIM 0 */
 
 export default function Input() {
   /* ADIM 1 */
-	
-  const inputuDeğiştir = evt => {
-    // When the input changes, its whole value can be found inside the event object.
-    // Log out the synthetic event object 'evt' and see for yourself.
+  const [inputDeğeri, setInputDeğeri] = useState("");
+
+  const inputuDeğiştir = (evt) => {
     const { value } = evt.target;
-	
+    setInputDeğeri(value);
     /* ADIM 4 */
+
+    if (value.length > 10) {
+      setInputRenk("crimson");
+    } else {
+      setInputRenk("royalblue");
+    }
   };
+
   const reset = () => {
+    setInputDeğeri("");
     /* ADIM 5 */
   };
 
+  const [inputRenk, setInputRenk] = useState("crimson");
+
   const stil = {
-    fontSize: '1.5em',
-    marginBottom: '0.3em',
-    color: 'crimson', /* ADIM 2 */
+    fontSize: "1.5em",
+    marginBottom: "0.3em",
+    color: inputRenk /* ADIM 2 */,
   };
 
   return (
-    <div className='widget-input container'>
+    <div className="widget-input container">
       <h2>Input</h2>
-      <div id='output' style={stil}></div> {/* ADIM 3 */}
+      <div id="output" style={stil}>
+        {inputDeğeri.toUpperCase()}
+      </div>{" "}
+      {/* ADIM 3 */}
       <div>
-		<input id='input' type='text' onChange={inputuDeğiştir} /> {/* ADIM 6 */}
-        <button id='resetInput' onClick={reset}>Reset</button>
+        <input
+          id="input"
+          type="text"
+          onChange={inputuDeğiştir}
+          value={inputDeğeri}
+        />{" "}
+        {/* ADIM 6 */}
+        <button id="resetInput" onClick={reset}>
+          Reset
+        </button>
       </div>
     </div>
   );
